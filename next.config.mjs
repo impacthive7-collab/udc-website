00eb776f90371f841ea740471c83e757eadc8d60
impacthive7-collab <impacthive7@gmail.com>
2026-05-27 09:40:18 +0300

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
+export default withNextIntl(nextConfig);
